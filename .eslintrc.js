96038f9ced228fcf4c698be2f53c9e056fbcf83e
QuyTranSMD <kurochan159357@gmail.com>
2023-01-04 09:38:20 +0700

    eslint, prettier, gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+    es2021: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true,
+        trailingComma: 'all',
+        singleQuote: true,
+        printWidth: 150,
+        tabWidth: 2,
+        endOfLine: 'auto',
+      },
+    ],
+    'import/extensions': 'off',
+    'no-console': 'off',
+  },
+};
